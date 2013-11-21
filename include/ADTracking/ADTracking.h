63a08f7f5b80e2da089afd92dba5cd22c539a359
szarajewski <vitaliy.sharaevskiy@gmail.com>
2013-11-21 16:49:31 +0400

    Initial commit

---
+++ include/ADTracking/ADTracking.h
@@ -0,0 +1,15 @@
+//
+//  ADTracking.h
+//  ADTracking
+//
+//  Created by szarajewski on 08.11.13.
+//  Copyright (c) 2013 "Online Publishing Partners" LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADTracking : NSObject
+
++ (void) setTargetID:(NSString *)targetID;
+
+@end
